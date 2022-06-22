c6c26350f45f154f98f6948c415d1c771620d56e
oliverjessner <oliver.jessner@gmail.com>
2022-06-22 11:55:44 +0200

    work in progress

--- dist/dev/bundle.js
+++ dist/dev/bundle.js
@@ -1128,45 +1128,48 @@ customElements.define('settings-menu', SettingsMenu);
 class Threads extends s {
     #icons = Object.freeze({
         user: 'user',
-        group: 'users-between-lines"></i>',
-        channel: 'object-intersect',
+        group: 'users-rectangle',
+        channel: 'object-ungroup',
     });
-    #activeThreads = [];
+
+    static properties = {
+        threads: [],
+    };
 
     constructor() {
         super();
+        this.threads = [];
     }
 
     createRenderRoot() {
         return this; // prevents creating a shadow root
     }
 
-    #_addThreads(threads) {
-        const HTML = threads.map(
-            ({
-                expire,
-                nickname,
-                sender,
-                status,
-                text,
-                threadID,
-                ts,
-                type,
-            }) => {
-                const icon = `<i class="fa-solid fa-${this.#icons[type]}"></i>`;
-                return `<div class="thread">${icon}<p>${nickname} <span>#${sender}<span></p></div>`;
-            }
-        );
+    render() {
+        const HTML = this.threads
+            .map(
+                ({
+                    expire,
+                    nickname,
+                    sender,
+                    status,
+                    text,
+                    threadID,
+                    ts,
+                    type,
+                }) => {
+                    const icon = `<i class="fa-solid fa-${
+                        this.#icons[type]
+                    }"></i>`;
+                    return `<div sender="${sender}" type="${type}" class="thread">${icon}<div><p>${nickname} <span class="float-right">#${sender}<span></p><p>${text.substring(
+                        0,
+                        23
+                    )} <span class="float-right">${ts}</span></p></div></div>`;
+                }
+            )
+            .join('');
 
         this.innerHTML = HTML;
-    }
-
-    addThreads(threads) {
-        if (!threads || threads.length === 0) {
-            return;
-        }
-
-        return this.#_addThreads(threads);
     }
 }
 
@@ -1321,17 +1324,50 @@ customElements.define('app-layout', AppLayout);
 document.addEventListener('DOMContentLoaded', function () {
     const threads = document.querySelector('threads-menu');
 
-    threads.addThreads([
+    threads.threads = [
         {
             expire: 'Tue Jul 19 2022 05:33:42 GMT+0000 (Coordinated Universal Time)',
             id: '10323',
             nickname: 'Bobo Macbook',
             sender: '10323',
             status: 'read',
+            text: 'Hallo Oli',
+            threadID: '10322:10323',
+            ts: 'Mon Jun 20 2022 06:01:34 GMT+0000 (Coordinated Universal Time)',
+            type: 'user',
+        },
+        {
+            expire: 'Tue Jul 19 2022 05:33:42 GMT+0000 (Coordinated Universal Time)',
+            id: '10323',
+            nickname: 'Orangen Fans',
+            sender: '13454',
+            status: 'read',
+            text: 'Wie hoch ist euer konsum?',
+            threadID: '10322:10323',
+            ts: 'Mon Jun 20 2022 06:01:34 GMT+0000 (Coordinated Universal Time)',
+            type: 'group',
+        },
+        {
+            expire: 'Tue Jul 19 2022 05:33:42 GMT+0000 (Coordinated Universal Time)',
+            id: '10323',
+            nickname: 'Verschwörung',
+            sender: '1',
+            status: 'read',
+            text: 'Kxc+a22sDxm3bDZj1jj7k2HA0dimrZ92dtFEGdLBC0JkRzEQ/m96583mIqAOHmIxqHJwMQ==',
+            threadID: '10322:10323',
+            ts: 'Mon Jun 20 2022 06:01:34 GMT+0000 (Coordinated Universal Time)',
+            type: 'channel',
+        },
+        {
+            expire: 'Tue Jul 19 2022 05:33:42 GMT+0000 (Coordinated Universal Time)',
+            id: '10323',
+            nickname: 'Oli',
+            sender: '1098765',
+            status: 'read',
             text: 'Kxc+a22sDxm3bDZj1jj7k2HA0dimrZ92dtFEGdLBC0JkRzEQ/m96583mIqAOHmIxqHJwMQ==',
             threadID: '10322:10323',
             ts: 'Mon Jun 20 2022 06:01:34 GMT+0000 (Coordinated Universal Time)',
             type: 'user',
         },
-    ]);
+    ];
 });
