d56ac73c6d039bbf9da08de990952c331d2a7439
bobo23 <bosobieski@gmail.com>
2022-07-06 10:39:48 +0200

    changes in destroy everything

--- test/cypress/e2e/desktop/panic/destroy_everything.cy.js
+++ test/cypress/e2e/desktop/panic/destroy_everything.cy.js
@@ -3,7 +3,7 @@ import setup from '../setup.js';
 describe('Trigger Destroy everything', () => {
     setup();
 
-    it('Enter wrong id shows error toas', () => {
+    it('Enter wrong id shows error toast', () => {
         cy.get('.bottom-navi .fa-bomb').click();
         cy.get('panic-modal-window .modal-content input').type(12345);
         cy.get('panic-modal-window .modal-content button').click();
@@ -18,12 +18,9 @@ describe('Trigger Destroy everything', () => {
         cy.get('toast-notification.active .progress.error').should('have.class', 'active');
     });
 
-    it('Enter correct id shows success toast', () => {
+    it('Enter correct id', () => {
         cy.get('.bottom-navi .fa-bomb').click();
         cy.get('panic-modal-window .modal-content input').type('1337');
-        cy.wait(1500);
         cy.get('panic-modal-window .modal-content button').click();
-        cy.get('toast-notification').should('have.class', 'active');
-        cy.get('toast-notification.active .progress.success').should('have.class', 'active');
     });
 });
