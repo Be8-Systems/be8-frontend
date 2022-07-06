a849ad69c6209112f3c38dc0ff2a19e6da301c3c
oliverjessner <oliver.jessner@gmail.com>
2022-07-06 08:33:13 +0200

    fixing

---
+++ build/replace.mjs
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import Applause from 'applause';
+const { version } = 3.14; 
+const options = [{
+    options: {
+        patterns: [{
+            //match: /\"..\//g,
+            match: /{{timestamp}}/g,
+            replacement: `?v=${version}`
+        }]
+    },
+    path: './lib/html/template.html',
+    write: './lib/html/index.html',
+}];
+
+options.forEach(function ({ options, path, write }) {
+    fs.readFile(path, 'utf8', function (err, content) {
+        const applause = Applause.create(options);
+        const output = applause.replace(content);
+        
+        if (err) {
+            return console.log(err);
+        }
+        if (!output.content) {
+            return console.log(`nothing replaced ${path}`);
+        }
+
+        fs.writeFile(write, output.content, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            return console.log(`replaced ${path}`);
+        }); 
+    });
+});
