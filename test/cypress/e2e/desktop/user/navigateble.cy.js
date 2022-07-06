4d6d3dff79a060ce7053edf660a88f7f89c12d0b
bobo23 <bosobieski@gmail.com>
2022-07-06 08:43:34 +0200

    added more tests

--- test/cypress/e2e/desktop/user/navigateble.cy.js
+++ test/cypress/e2e/desktop/user/navigateble.cy.js
@@ -3,8 +3,7 @@ import setup from '../setup.js';
 describe('Check if menu or modal is navigateable', () => {
     setup();
 
-    // close modal and check if modal is gone
-    it('Check different classes and elements', () => {
+    it('Check if user menu is viewable and hidden again', () => {
         cy.get('header .fa-circle-user').click();
         cy.get('user-menu').should('not.have.class', 'hide');
         cy.get('.bottom-navi .fa-comments').click();
--- test/cypress/e2e/mobile/user/navigateble.cy.js
+++ test/cypress/e2e/mobile/user/navigateble.cy.js
@@ -3,8 +3,10 @@ import setup from '../setup.js';
 describe('Check if menu or modal is navigateable', () => {
     setup();
 
-    // close modal and check if modal is gone
-    it('Check different classes and elements', () => {
-        throw new Error('ToDo: implement test');
+    it('Check if user menu is viewable and hidden again', () => {
+        cy.get('header .fa-circle-user').click();
+        cy.get('user-menu').should('not.have.class', 'hide');
+        cy.get('.bottom-navi .fa-comments').click();
+        cy.get('user-menu').should('have.class', 'hide');
     });
 });
