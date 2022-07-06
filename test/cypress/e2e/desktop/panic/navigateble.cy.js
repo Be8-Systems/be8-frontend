7a1907562397971517e50929efc1b827cc849f1e
bobo23 <bosobieski@gmail.com>
2022-07-06 08:09:06 +0200

    working on cypress tests

--- test/cypress/e2e/desktop/panic/navigateble.cy.js
+++ test/cypress/e2e/desktop/panic/navigateble.cy.js
@@ -3,7 +3,10 @@ import setup from '../setup.js';
 describe('Check if menu or modal is navigateable', () => {
     setup();
 
-    it('Check different classes and elements', () => {
-        throw new Error('ToDo: implement test');
+    it('Check if modal is viewable and hidden again after closing', () => {
+        cy.get('.bottom-navi .fa-bomb').click();
+        cy.get('panic-modal-window').should('not.have.class', 'hide');
+        cy.get('panic-modal-window .close-modal').click();
+        cy.get('panic-modal-window').should('have.class', 'hide');
     });
 });
--- test/cypress/e2e/desktop/settings/navigateble.cy.js
+++ test/cypress/e2e/desktop/settings/navigateble.cy.js
@@ -3,7 +3,10 @@ import setup from '../setup.js';
 describe('Check if menu or modal is navigateable', () => {
     setup();
 
-    it('Check different classes and elements', () => {
-        throw new Error('ToDo: implement test');
+    it('Check if settings menu is viewable and hidden again', () => {
+        cy.get('.bottom-navi .fa-gears').click();
+        cy.get('settings-menu').should('not.have.class', 'hide');
+        cy.get('.bottom-navi .fa-comments').click();
+        cy.get('settings-menu').should('have.class', 'hide');
     });
 });
