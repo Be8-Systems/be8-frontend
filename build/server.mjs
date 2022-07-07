7e18d724ef4ceb6ec112cf09867c1fa323b88269
bobo23 <bosobieski@gmail.com>
2022-07-07 16:21:53 +0200

    new token handling

--- build/server.mjs
+++ build/server.mjs
@@ -2,7 +2,12 @@ import start from 'be8-backend';
 
 start({
     staticFiles: './dist/',
-    fakeTokens: [
-        '12345678901234567890123456789012'
-    ]
+    fakeTokens: [{
+        token: '12345678901234567890123456789012',
+        type: 'endless'
+    }, {
+        token: '12345678901234567890123456789010',
+        type: 'promo',
+        validTime: 15768000 // half a year in seconds
+    }]
 });
