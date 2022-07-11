bf4bb86e0364340ebd50fc005bae1b525c5cdc9e
oliverjessner <oliver.jessner@gmail.com>
2022-07-11 17:02:20 +0200

    merge fix

--- dist/dev/bundle.js
+++ dist/dev/bundle.js
@@ -9364,47 +9364,51 @@ const GET = {
 };
 
 const connection = indexedDB.open('be8', 1);
-const initialiseDB = new Promise(function (success, error) {
-    connection.onupgradeneeded = function () {
-        const db = connection.result;
-        const publicKeysStore = db.createObjectStore('publicKeys', {
-            keyPath: 'accID',
-        });
-        const privateKeysStore = db.createObjectStore('privateKeys', {
-            keyPath: 'accID',
-        });
-        const groupKeysStore = db.createObjectStore('groupKeys', {
-            keyPath: ['groupID', 'version'],
-        });
-        const indexs = [
-            ['crv', 'crv', { unique: false }],
-            ['x', 'x', { unique: false }],
-            ['y', 'y', { unique: false }],
-            ['kty', 'kty', { unique: false }],
-            ['key_ops', 'key_ops', { unique: false }],
-            ['ext', 'ext', { unique: false }],
-        ];
+function initialiseDB() {
+    return new Promise(function (success, error) {
+        connection.onupgradeneeded = function () {
+            const db = connection.result;
+            const publicKeysStore = db.createObjectStore('publicKeys', {
+                keyPath: 'accID',
+            });
+            const privateKeysStore = db.createObjectStore('privateKeys', {
+                keyPath: 'accID',
+            });
+            const groupKeysStore = db.createObjectStore('groupKeys', {
+                keyPath: ['groupID', 'version'],
+            });
+            const indexs = [
+                ['crv', 'crv', { unique: false }],
+                ['x', 'x', { unique: false }],
+                ['y', 'y', { unique: false }],
+                ['kty', 'kty', { unique: false }],
+                ['key_ops', 'key_ops', { unique: false }],
+                ['ext', 'ext', { unique: false }],
+            ];
 
-        indexs.forEach(function (parameters) {
-            publicKeysStore.createIndex(...parameters);
-            privateKeysStore.createIndex(...parameters);
-            groupKeysStore.createIndex(...parameters);
-        });
-        return success();
-    };
+            indexs.forEach(function (parameters) {
+                publicKeysStore.createIndex(...parameters);
+                privateKeysStore.createIndex(...parameters);
+                groupKeysStore.createIndex(...parameters);
+            });
 
-    connection.onsuccess = function () {
-        return success();
-    };
+            return success();
+        };
 
-    connection.onerror = function (event) {
-        console.log(event);
-        return error();
-    };
-});
+        connection.onsuccess = function (event) {
+            console.log(event);
+            return success();
+        };
+
+        connection.onerror = function (event) {
+            console.log(event);
+            return error();
+        };
+    });
+}
 
 async function initialiseDB$1() {
-    await initialiseDB;
+    await initialiseDB();
     return connection;
 }
 
