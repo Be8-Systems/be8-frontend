b5afd75558bd1c10188c52e56f6ae412fa1b123e
oliverjessner <oliver.jessner@gmail.com>
2022-07-26 16:42:53 +0200

    adding static height to prevent wrong scrolling

--- dist/dev/serviceworker.js
+++ dist/dev/serviceworker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'v0.0.100';
-const RUNTIME = 'v0.0.100';
+const PRECACHE = 'v0.0.101';
+const RUNTIME = 'v0.0.101';
 const PRECACHE_URLS = Object.freeze([
     '/',
     'bundle.css'
