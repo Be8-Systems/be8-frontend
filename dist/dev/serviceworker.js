a87e95a910946fed5f33ca0e96474db722150ff5
oliverjessner <oliver.jessner@gmail.com>
2022-07-28 09:04:39 +0200

    fix for android raw

--- dist/dev/serviceworker.js
+++ dist/dev/serviceworker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'v0.0.117';
-const RUNTIME = 'v0.0.117';
+const PRECACHE = 'v0.0.118';
+const RUNTIME = 'v0.0.118';
 const PRECACHE_URLS = Object.freeze([
     '/',
     'bundle.css'
