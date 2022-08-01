1595acc2042a21bd01e326ebe64e94bf687c0732
bobo23 <bosobieski@gmail.com>
2022-08-01 08:34:08 +0200

    version bump

--- dist/prod/serviceworker.js
+++ dist/prod/serviceworker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'v0.0.131';
-const RUNTIME = 'v0.0.131';
+const PRECACHE = 'v0.0.132';
+const RUNTIME = 'v0.0.132';
 const PRECACHE_URLS = Object.freeze([
     '/',
     'bundle.css'
