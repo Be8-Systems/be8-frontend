decbc5a995cb5ecdd84ad5641d2ec51f8e73eef1
oliverjessner <oliver.jessner@gmail.com>
2022-08-02 14:15:14 +0200

    click away on modal

--- dist/prod/serviceworker.js
+++ dist/prod/serviceworker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'v0.0.140';
-const RUNTIME = 'v0.0.140';
+const PRECACHE = 'v0.0.142';
+const RUNTIME = 'v0.0.142';
 const PRECACHE_URLS = Object.freeze([
     '/',
     'bundle.css'
